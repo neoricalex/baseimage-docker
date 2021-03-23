4014a9eb106f0cac2316e81cc6d54429e76b7094
neoricalex <neo.webmaster.2@gmail.com>
2021-03-23 11:02:10 -0300

    Atualização Automática via bash shell

--- vagrant-libs/requerimentos_box_base.sh
+++ vagrant-libs/requerimentos_box_base.sh
@@ -17,7 +17,34 @@ sudo apt-get install -y \
     autoconf \
     build-essential \
     make \
-    virtualbox virtualbox-guest-dkms virtualbox-guest-additions-iso \
-	bridge-utils dnsmasq-base ebtables libvirt-daemon-system libvirt-clients \
-    libvirt-dev qemu-kvm qemu-utils ruby-dev \
-    ruby-libvirt libxslt-dev libxml2-dev zlib1g-dev	
+    virtualbox virtualbox-guest-dkms virtualbox-guest-additions-iso
+
+echo "==> Adicionar o grupo kvm"
+sudo groupadd kvm
+
+echo "==> Adicionar o usuário vagrant ao grupo kvm"
+sudo usermod -aG kvm vagrant
+
+echo "==> Instalar os pacotes do kvm"
+sudo apt install -y qemu-system qemu qemu-kvm qemu-utils qemu-block-extra \
+					libvirt-daemon libvirt-daemon-system libvirt-clients \
+					cpu-checker libguestfs-tools libosinfo-bin \
+					bridge-utils dnsmasq-base ebtables libvirt-dev ruby-dev \
+					ruby-libvirt libxslt-dev libxml2-dev zlib1g-dev	
+
+echo "==> Adicionar o usuário vagrant ao grupo libvirt"
+sudo usermod -aG libvirt vagrant
+
+echo "==> Iniciar o serviço KVM de forma automática"
+sudo systemctl start libvirtd
+sudo systemctl enable --now libvirtd
+
+echo "==> Reiniciar o serviço libvirt"
+sudo systemctl restart libvirtd.service
+
+echo "==> Habilitar o IPv4 e IPv6 forwarding"
+sudo sed -i "/net.ipv4.ip_forward=1/ s/# *//" /etc/sysctl.conf
+sudo sed -i "/net.ipv6.conf.all.forwarding=1/ s/# *//" /etc/sysctl.conf
+
+echo "==> Aplicar as mudanças"
+sudo sysctl -p
