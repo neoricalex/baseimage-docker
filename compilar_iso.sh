969e0e87aaca84e2b3cbdcae14232344a0fd529f
neoricalex <neo.webmaster.2@gmail.com>
2021-03-23 13:41:40 -0300

    Atualização Automática via bash shell

--- compilar_iso.sh
+++ compilar_iso.sh
@@ -9,6 +9,8 @@ export NFDOS_VERSAO="0.4.4"
 export NFDOS_ROOT=$NFDOS_HOME/core
 export NFDOS_ROOTFS=$NFDOS_ROOT/rootfs
 export NFDOS_DISCO=$NFDOS_ROOT/nfdos.img
+# Vagrant
+export VERSAO_BOX_VAGRANT="libvirt"
 
 echo "Iniciando a criação da imagem ISO do NFDOS $NFDOS_VERSAO ..."
 
@@ -51,31 +53,39 @@ else
     echo "A $NFDOS_ROOT/nfdos.iso existe"
 fi
 
-echo "Checkando se a $NFDOS_HOME/desktop/vagrant/NFDOS-$NFDOS_VERSAO.box existe"
-if [ ! -f "$NFDOS_HOME/desktop/vagrant/NFDOS-$NFDOS_VERSAO.box" ]; then
+if [ "$VERSAO_BOX_VAGRANT" == "virtualbox" ]; 
+then
+	echo "Checkando se a $NFDOS_HOME/desktop/vagrant/virtualbox/NFDOS-$NFDOS_VERSAO.box existe..."
+	if [ ! -f "$NFDOS_HOME/desktop/vagrant/virtualbox/NFDOS-$NFDOS_VERSAO.box" ]; 
+	then
+		echo "A $NFDOS_HOME/desktop/vagrant/virtualbox/NFDOS-$NFDOS_VERSAO.box não existe. Criando ela..."
 
-    echo "A $NFDOS_HOME/desktop/vagrant/NFDOS-$NFDOS_VERSAO.box não existe. Criando ela..."
+		echo "Checkando o SHA256 da imagem ISO..."
+		checkar_sha256=$(sha256sum $NFDOS_ROOT/nfdos.iso | awk '{ print $1 }')
+		jq ".variables.iso_checksum = \"$checkar_sha256\"" $NFDOS_HOME/desktop/virtualbox.json | sponge $NFDOS_HOME/desktop/virtualbox.json
 
-    echo "Checkando o SHA256 da imagem ISO..."
-    checkar_sha256=$(sha256sum $NFDOS_ROOT/nfdos.iso | awk '{ print $1 }')
-    jq ".variables.iso_checksum = \"$checkar_sha256\"" $NFDOS_HOME/desktop/virtualbox.json | sponge $NFDOS_HOME/desktop/virtualbox.json
+		cd $NFDOS_HOME/desktop
+		packer build virtualbox.json #VBoxManage setextradata VM-name "VBoxInternal/TM/TSCTiedToExecution" 1
+		cd $NEORICALEX_HOME
+	fi
+	echo "A $NFDOS_HOME/desktop/vagrant/virtualbox/NFDOS-$NFDOS_VERSAO.box existe."
 
-    cd $NFDOS_HOME/desktop
-    packer build virtualbox.json #VBoxManage setextradata VM-name "VBoxInternal/TM/TSCTiedToExecution" 1
-    cd $NEORICALEX_HOME
-elif [ ! -f "$NFDOS_HOME/desktop/vagrant/libvirt/NFDOS-$NFDOS_VERSAO.box" ]; then
+elif [ "$VERSAO_BOX_VAGRANT" == "libvirt" ];
+then
+	echo "Checkando se a $NFDOS_HOME/desktop/vagrant/libvirt/NFDOS-$NFDOS_VERSAO.box existe..."
+	if [ ! -f "$NFDOS_HOME/desktop/vagrant/libvirt/NFDOS-$NFDOS_VERSAO.box" ]; then
 
-    echo "A $NFDOS_HOME/desktop/vagrant/libvirt/NFDOS-$NFDOS_VERSAO.box não existe. Criando ela..."
+		echo "A $NFDOS_HOME/desktop/vagrant/libvirt/NFDOS-$NFDOS_VERSAO.box não existe. Criando ela..."
 
-    echo "Checkando o SHA256 da imagem ISO..."
-    checkar_sha256=$(sha256sum $NFDOS_ROOT/nfdos.iso | awk '{ print $1 }')
-    jq ".variables.iso_checksum = \"$checkar_sha256\"" $NFDOS_HOME/desktop/libvirt.json | sponge $NFDOS_HOME/desktop/libvirt.json
+		echo "Checkando o SHA256 da imagem ISO..."
+		checkar_sha256=$(sha256sum $NFDOS_ROOT/nfdos.iso | awk '{ print $1 }')
+		jq ".variables.iso_checksum = \"$checkar_sha256\"" $NFDOS_HOME/desktop/libvirt.json | sponge $NFDOS_HOME/desktop/libvirt.json
 
-    cd $NFDOS_HOME/desktop
-    packer build libvirt.json # PACKER_LOG=1
-    cd $NEORICALEX_HOME
-
+		cd $NFDOS_HOME/desktop
+		packer build libvirt.json # PACKER_LOG=1
+		cd $NEORICALEX_HOME
+	fi
+	echo "Checkando se a $NFDOS_HOME/desktop/vagrant/libvirt/NFDOS-$NFDOS_VERSAO.box existe."
 else
-    echo "A $NFDOS_HOME/desktop/vagrant/NFDOS-$NFDOS_VERSAO.box existe."
-
+    echo "A versão $VERSAO_BOX_VAGRANT não é suportada."
 fi
