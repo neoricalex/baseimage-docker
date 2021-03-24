6ab500f6dedc64d60747637b5eceb9c1ae2a7c52
neoricalex <neo.webmaster.2@gmail.com>
2021-03-23 22:32:43 -0300

    Atualização Automática via bash shell

--- compilar_vps.sh
+++ compilar_vps.sh
@@ -98,7 +98,7 @@ provisionar_vps(){
 		if [ $? == "1" ];
 		then
 			vagrant cloud auth login
-			vagrant package --base neoricalex/ubuntu --output vagrant-libs/vps_dev.box
+			vagrant package --base ubuntu/focal64 --output vagrant-libs/vps_dev.box
 			exit
 			#vagrant cloud publish \
 			#--box-version $NFDOS_VERSAO \
