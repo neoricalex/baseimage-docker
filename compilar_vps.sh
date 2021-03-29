0ae8116f1bf009610b5e6b7c6854ba45869b5ff6
neoricalex <neo.webmaster.2@gmail.com>
2021-03-29 14:19:31 -0300

    Atualização Automática via bash shell

--- compilar_vps.sh
+++ compilar_vps.sh
@@ -27,7 +27,33 @@ echo "==> Instalar pacotes para desenvolvimento geral..."
 sudo apt-get install -y build-essential checkinstall libreadline-gplv2-dev \
 	libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev \
 	libbz2-dev libffi-dev python3-pip unzip lsb-release software-properties-common \
-	curl wget git rsync devscripts python-dev python3-venv make
+	curl wget git rsync devscripts python-dev python3-venv
+
+echo "==> Instalar pacotes para a criação da imagem ISO..."
+sudo apt install -y \
+	binutils \
+	debootstrap \
+	squashfs-tools \
+	xorriso \
+	grub-pc-bin \
+	grub-efi-amd64-bin \
+	unzip \
+	mtools \
+	whois \
+	jq \
+	moreutils \
+	make
+
+echo "==> Instalar os pacotes do kvm"
+sudo apt install -y qemu-system qemu qemu-kvm qemu-utils qemu-block-extra \
+					libvirt-daemon libvirt-daemon-system libvirt-clients \
+					cpu-checker libguestfs-tools libosinfo-bin \
+					bridge-utils dnsmasq-base ebtables libvirt-dev ruby-dev \
+					ruby-libvirt libxslt-dev libxml2-dev zlib1g-dev       
+
+sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
+sudo apt-get update
+sudo apt install -y build-dep #qemu-user-static libvirt-bin 
 
 echo "==> Instalar o VirtualBox"
 echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian focal contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
@@ -35,7 +61,7 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install virtualbox-6.1 -y
-sudo apt install -y virtualbox-guest-dkms #virtualbox-guest-x11
+sudo apt install -y virtualbox-guest-dkms virtualbox-guest-x11
 sudo apt install -y virtualbox-guest-additions-iso
 
 echo "==> Instalar o Extension Pack do VirtualBox"
@@ -56,42 +82,16 @@ sudo apt-get install -y docker-ce docker-compose
 sudo apt-get remove -y golang-docker-credential-helpers
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo echo '{"experimental": true}' > /etc/docker/daemon.json
-service docker restart
+sudo service docker restart
 
 echo "==> Adicionar o usuário vagrant ao grupo docker"
 sudo usermod -aG docker vagrant
-
-echo "==> Instalar pacotes para a criação da imagem ISO..."
-sudo apt install -y \
-	binutils \
-	debootstrap \
-	squashfs-tools \
-	xorriso \
-	grub-pc-bin \
-	grub-efi-amd64-bin \
-	unzip \
-	mtools \
-	whois \
-	jq \
-	moreutils \
-	make
 
 echo "==> Adicionar o grupo kvm"
 sudo groupadd kvm
 
 echo "==> Adicionar o usuário vagrant ao grupo kvm"
 sudo usermod -aG kvm vagrant
-
-echo "==> Instalar os pacotes do kvm"
-sudo apt install -y qemu-system qemu qemu-kvm qemu-utils qemu-block-extra \
-					libvirt-daemon libvirt-daemon-system libvirt-clients \
-					cpu-checker libguestfs-tools libosinfo-bin \
-					bridge-utils dnsmasq-base ebtables libvirt-dev ruby-dev \
-					ruby-libvirt libxslt-dev libxml2-dev zlib1g-dev       
-
-sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
-sudo apt-get update
-sudo apt install -y build-dep #qemu-user-static libvirt-bin 
 
 echo "==> Adicionar o usuário vagrant ao grupo libvirt"
 sudo usermod -aG libvirt vagrant
