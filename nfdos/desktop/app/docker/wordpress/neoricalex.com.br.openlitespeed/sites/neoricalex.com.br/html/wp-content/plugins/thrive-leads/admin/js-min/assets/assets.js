1664c15f7301ecfb3a69f687099cbd7073b24547
neoricalex <neo.webmaster.2@gmail.com>
2021-04-03 13:12:51 -0300

    Adicionar Módulo Ansible

---
+++ nfdos/desktop/app/docker/wordpress/neoricalex.com.br.openlitespeed/sites/neoricalex.com.br/html/wp-content/plugins/thrive-leads/admin/js-min/assets/assets.js
@@ -0,0 +1,5 @@
+/*! Thrive Leads - The ultimate Lead Capture solution for wordpress - 2021-01-22
+* https://thrivethemes.com 
+* Copyright (c) 2021 * Thrive Themes */
+
+var ThriveLeads=ThriveLeads||{};jQuery(document).ready(function(){ThriveLeads.router.navigate("#assets",{trigger:!0})});
