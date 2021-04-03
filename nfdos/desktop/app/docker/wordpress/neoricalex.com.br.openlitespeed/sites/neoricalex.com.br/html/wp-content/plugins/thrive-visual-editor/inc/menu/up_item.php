b231b7e9a4223fc92dd8d44a1e2f8a39beb4374b
neoricalex <neo.webmaster.2@gmail.com>
2021-04-03 13:13:15 -0300

    Adicionar Módulo Ansible

---
+++ nfdos/desktop/app/docker/wordpress/neoricalex.com.br.openlitespeed/sites/neoricalex.com.br/html/wp-content/plugins/thrive-visual-editor/inc/menu/up_item.php
@@ -0,0 +1,17 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Silence is golden
+}
+
+?>
+
+<div id="tve-up_item-component" class="tve-component" data-view="UPItem">
+	<div class="dropdown-header" data-prop="docked">
+		<?php echo __( 'Main Options', 'thrive-cb' ); ?>
+		<i></i>
+	</div>
+	<div class="dropdown-content">
+		<div class="tve-control full-width" data-view="FieldsLabel"></div>
+	</div>
+</div>
+
